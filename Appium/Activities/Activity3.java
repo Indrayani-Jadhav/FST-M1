1b8df528f09b495632229f8bb9cdabc88af16279
Indrayani-Jadhav <112160166+Indrayani-Jadhav@users.noreply.github.com>
2022-11-11 18:20:25 +0530

    Add files via upload

---
+++ Appium/Activities/Activity3.java
@@ -0,0 +1,107 @@
+//Calculator operation-Addition/Substarction/Multiplication/Division in Emulator
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class Activity3 {
+    // Driver Declaration
+    AndroidDriver driver;
+
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.calculator2");
+        options.setAppActivity(".Calculator");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+    }
+
+    // Test method
+    @Test
+    public void additionTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("plus")).click();
+        driver.findElement(AppiumBy.id("digit_9")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the res
+        String res = driver.findElement(AppiumBy.id("res")).getText();
+
+        // Assertion
+        Assert.assertEquals(res, "14");
+    }
+
+    // Test method
+    @Test
+    public void subtractTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("minus")).click();
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String res = driver.findElement(AppiumBy.id("res")).getText();
+
+        // Assertion
+        Assert.assertEquals(res, "5");
+    }
+
+    // Test method
+    @Test
+    public void multiplyTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        driver.findElement(AppiumBy.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the res
+        String res = driver.findElement(AppiumBy.id("res")).getText();
+
+        // Assertion
+        Assert.assertEquals(res, "500");
+    }
+
+    // Test method
+    @Test
+    public void divideTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("divide")).click();
+        driver.findElement(AppiumBy.id("digit_2")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the res
+        String res = driver.findElement(AppiumBy.id("res")).getText();
+
+        // Assertion
+        Assert.assertEquals(res, "25");
+    }
+
+    // Tear down method
+    @AfterClass
+    public void tearDown() {
+        // Close the app
+        driver.quit();
+    }
+}
